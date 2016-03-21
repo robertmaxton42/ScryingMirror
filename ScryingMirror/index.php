cf64610ae7b53a8cb3ca0c3af3baca31a07b5437
Robert Maxton <robert.maxton42@gmail.com>
2016-03-20 22:31:25 -0400

    Get html.innerhtml and rebase.

--- ScryingMirror/index.php
+++ ScryingMirror/index.php
@@ -5,6 +5,8 @@
     
     $PageContents = urldecode($_POST['PageContents']);
     $dom->loadHTML($PageContents);
+    $baseTag = $dom->createElement("base");
+    $baseTag->setAttribute('href', urldecode($_POST['URL']));
+    $dom->getElementsByTagName('head')->item(0)->appendChild($baseTag);
     $dom->saveHTMLFile('latestpage.html');
-	echo urldecode($_POST['url']) . " has been saved."
 ?>
