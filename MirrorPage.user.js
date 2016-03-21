0f50dbb3375da568b2f80313203268b894b08824
Robert Maxton <robert.maxton42@gmail.com>
2016-03-21 13:07:15 -0400

    heroku initial commit

---
+++ MirrorPage.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name        Mirror Page
+// @namespace   mailto:linkhyrule5@gmail.com
+// @description POSTs page to dynamic page mirror
+// @include     http://*.syosetu.com/*
+// @version     1
+// @grant       GM_xmlhttpRequest
+// ==/UserScript==
+
+var ihtml = document.getElementsByTagName("html")[0].innerHTML;
+GM_xmlhttpRequest({
+    method: 'POST',
+    url: 'http://localhost:26553/index.php',
+    data: "PageContents=" + encodeURIComponent(ihtml) + "&URL=" + encodeURIComponent(document.URL),
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+});
+
+window.setTimeout(function(){window.location.href = http://localhost:26553/latest}
