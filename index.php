5acfb3ba767ecdc06ef75c7c9b52e9274e0dbb91
Robert Maxton <robert.maxton42@gmail.com>
2016-03-23 01:42:30 -0400

    Untested link-rewrite draft.

--- index.php
+++ index.php
@@ -6,10 +6,11 @@
     $dom->formatOutput = true;
     
     //Get the webpage by POST from the GreaseMonkey plugin
-    //Parse into a DOM, and grab the head tag while we're at it.
+    //Parse into a DOM, and grab the head and body tags while we're at it.
     $PageContents = urldecode($_POST['PageContents']);
     $dom->loadHTML($PageContents);
     $head = $dom->getElementsByTagName('head')->item(0);
+    $body = $dom->getElementsByTagName('body')->item(0);
 
     //Rebase relative links.
     //Google Translate duplicates this work, I think.
@@ -31,14 +32,28 @@
 
     //Convert to JSON
     $jsLinks = json_encode($hrefs);
-    $scriptdata = "var links = " . $jsLinks . ";\n";
-    $scriptText = $dom->createTextNode($scriptdata);
-    $scriptTag = $dom->createElement('script');
+    $hrefsJSON = "var links = " . $jsLinks . ";\n";
+    $hrefsScriptNode = $dom->createTextNode($hrefsJSON);
+    $hrefsScriptTag = $dom->createElement('script');
     
-    $scriptTag->appendChild($scriptText);
-    $head->appendChild($scriptTag);
+    $hrefsScriptTag->appendChild($hrefsScriptNode);
+    $head->appendChild($hrefsScriptTag);
 
     //Inject javascript: rewrite links in order to their proper destinations.
+    $rewriteScript =
+        "function rewriteLinks() {
+            var aTags = document.getElementsByTagName('a');
+
+            for (i = 0; i < aTags.length; i++)
+                aTags[i].setAttribute('href', links[i]);
+        }";
+    $rewriteScriptNode = $dom->createTextNode($rewriteScript);
+    $rewriteScriptTag = $dom->createElement('script');
+    $rewriteScriptTag->appendChild($rewriteScriptNode);
+    $head->appendChild($rewriteScriptTag);
+
+    //Call rewriteLinks() onLoad
+    $body->setAttributes('onLoad', 'rewriteLinks()');
 
     //Save the modified webpage
     $dom->saveHTMLFile('latestpage.html');
